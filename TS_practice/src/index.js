0b1e98562b1997b42d6993d4907deb7766e5b6e5
siltea <siltea2019@gmail.com>
2024-09-24 00:25:10 +0900

    feat: 기본타입 실습코드 작성

---
+++ TS_practice/src/index.js
@@ -0,0 +1,38 @@
+// string
+var userName = 'Su Min';
+function greeting(name) {
+    return "Hello, ".concat(name, "!");
+}
+greeting(userName);
+// number
+var userAge = 32;
+// boolean
+var birthYear = null;
+if (userAge === 32) {
+    birthYear = true;
+    console.log("\uB2F9\uC2E0\uC740 ".concat(userAge, "\uC0B4, 1992\uB144\uC0DD \uC6D0\uC22D\uC774\uB760 \uC785\uB2C8\uB2E4"));
+}
+else {
+    birthYear = false;
+    console.log("\uB2F9\uC2E0\uC740 ".concat(userAge, "\uC0B4, 1992\uB144\uC0DD \uC6D0\uC22D\uC774\uB760\uAC00 \uC544\uB2C8\uAD70\uC694"));
+}
+// null
+var userData = null;
+function fetchUserData() {
+    userData = 'Su Min Kim';
+    console.log("User data fetched: ".concat(userData));
+}
+if (userData === null) {
+    console.log('User data is not available yet.');
+}
+else {
+    console.log("Welcome, ".concat(userName));
+}
+fetchUserData();
+// any
+var obj = { x: 0 };
+// obj.foo();
+// obj();
+obj.bar = 100;
+obj = 'hello';
+var n = obj;
---
+++ TS_practice/src/index.ts
@@ -0,0 +1,47 @@
+// string
+
+let userName: string = 'Su Min';
+
+function greeting(name: string) {
+  return `Hello, ${name}!`;
+}
+
+greeting(userName);
+
+// number
+let userAge: Number = 32;
+
+// boolean
+let birthYear: boolean | null = null;
+
+if (userAge === 32) {
+  birthYear = true;
+  console.log(`당신은 ${userAge}살, 1992년생 원숭이띠 입니다`);
+} else {
+  birthYear = false;
+  console.log(`당신은 ${userAge}살, 1992년생 원숭이띠가 아니군요`);
+}
+
+// null
+let userData: string | null = null;
+
+function fetchUserData() {
+  userData = 'Su Min Kim';
+  console.log(`User data fetched: ${userData}`);
+}
+
+if (userData === null) {
+  console.log('User data is not available yet.');
+} else {
+  console.log(`Welcome, ${userName}`);
+}
+
+fetchUserData();
+
+// any
+let obj: any = { x: 0 };
+// obj.foo();
+// obj();
+obj.bar = 100;
+obj = 'hello';
+const n: number = obj;
